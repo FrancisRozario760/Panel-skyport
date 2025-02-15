1d8b3cb35e4355fe0b4f23baaa140842b5f7640d
Francis Rozario <benedickrozario04@gmail.com>
2025-02-15 22:05:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 const { exec } = require("child_process");
-const express = require("express");
 
-const app = express();
-let sshInfo = "Starting tmate session... Please wait.";
+module.exports = async (req, res) => {
+    let sshInfo = "Starting tmate session... Please wait.";
 
-function startTmateSession() {
     exec("tmate -S /tmp/tmate.sock new-session -d", (err) => {
         if (err) {
             sshInfo = "Error starting tmate session: " + err.message;
-            return;
-        }
-        
-        exec("tmate -S /tmp/tmate.sock wait tmate-ready", () => {
-            exec("tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'", (err, stdout) => {
-                if (err) {
-                    sshInfo = "Error retrieving SSH info: " + err.message;
-                    return;
-                }
-                sshInfo = `SSH Access: <br><pre>${stdout}</pre>`;
+        } else {
+            exec("tmate -S /tmp/tmate.sock wait tmate-ready", () => {
+                exec("tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'", (err, stdout) => {
+                    if (err) {
+                        sshInfo = "Error retrieving SSH info: " + err.message;
+                    } else {
+                        sshInfo = `SSH Access: <br><pre>${stdout}</pre>`;
+                    }
+                });
             });
-        });
+        }
     });
-}
 
-// Start the tmate session
-startTmateSession();
-
-app.get("/", (req, res) => {
-    res.send(`
+    res.setHeader("Content-Type", "text/html");
+    res.status(200).send(`
         <html>
             <head><title>Vercel SSH Access</title></head>
             <body>
@@ -36,6 +29,4 @@ app.get("/", (req, res) => {
             </body>
         </html>
     `);
-});
-
-app.listen(3000, () => console.log("Server running on port 3000"));
+};
